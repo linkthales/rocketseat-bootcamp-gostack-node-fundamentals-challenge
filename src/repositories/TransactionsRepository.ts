360928fec036cc0527a9c0629c2e86562c8c2f0b
Thales Martins <linkthales@hotmail.com>
2020-04-30 20:41:09 -0300

    feat: 🎸 Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce<Balance>(
+      (currentBalance, transaction) =>
+        transaction.type === 'income'
+          ? {
+              ...currentBalance,
+              income: currentBalance.income + transaction.value,
+              total: currentBalance.total + transaction.value,
+            }
+          : {
+              ...currentBalance,
+              outcome: currentBalance.outcome + transaction.value,
+              total: currentBalance.total - transaction.value,
+            },
+      { income: 0, outcome: 0, total: 0 },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
